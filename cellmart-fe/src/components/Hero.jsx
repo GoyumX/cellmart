5d961139f34747f0696aadc03a16476326c58f06
Goyumx <goyum.doingcode@gmail.com>
2025-07-11 22:57:50 +0530

    chore: install shadcn/ui

---
+++ cellmart-fe/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function Hero() {
+
+    return (
+    <Button
+            type="submit"
+            className="rounded-full w-48 flex items-center gap-x-2 lg:h-12"
+          >
+            <span>CELLMART</span>
+        </Button>       
+    )
+}
