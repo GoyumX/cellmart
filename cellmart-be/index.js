533da3b18ff7c1a35f730cad56b21097ce497351
Goyumx <goyum.doingcode@gmail.com>
2025-07-16 23:08:16 +0530

    feat: initialize express server

--- cellmart-be/index.js
+++ cellmart-be/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 8000;
+app.listen(PORT, console.log(`Server is running on port ${PORT}...`));
