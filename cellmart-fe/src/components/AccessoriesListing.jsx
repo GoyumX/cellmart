e4b8df6cbd3f5f5574037e41d86946af7b101ff5
Goyumx <goyum.doingcode@gmail.com>
2025-07-26 23:52:54 +0530

    feat: add loading and error state in Home page

--- cellmart-fe/src/components/AccessoriesListing.jsx
+++ cellmart-fe/src/components/AccessoriesListing.jsx
@@ -19,6 +19,56 @@ export default function AccessoriesListing() {
     return accessories.type.toLowerCase().includes(selectedtypes.toLowerCase());
   })
 
+  if(isError){
+    return (
+      <section className="px-8 py-3 ">
+        <div className="mb-5">
+          <h2 className="text-3xl md:text-4xl font-bold mb-4">
+            Top trending Accessories
+          </h2>
+          <p className="text-lg text-muted-foreground">
+            Choose the most reliable and effcient accessories here at Cellmart
+          </p>
+        </div>
+        <div className="flex items-center gap-x-4">
+          {
+            types.map((types) => {
+              return (<BrandTab selectedtypes={selectedtypes} name={types} onClick={handleSelectedtypes} />)
+            })
+          }
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-4">
+        <p>Error Loading</p>
+        </div>
+      </section>
+    );
+  }
+
+  if(isLoading){
+    return (
+      <section className="px-8 py-3 ">
+        <div className="mb-5">
+          <h2 className="text-3xl md:text-4xl font-bold mb-4">
+            Top trending Accessories
+          </h2>
+          <p className="text-lg text-muted-foreground">
+            Choose the most reliable and effcient accessories here at Cellmart
+          </p>
+        </div>
+        <div className="flex items-center gap-x-4">
+          {
+            types.map((types) => {
+              return (<BrandTab selectedtypes={selectedtypes} name={types} onClick={handleSelectedtypes} />)
+            })
+          }
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-4">
+          <p>Loading...</p>
+        </div>
+      </section>
+    );
+  }
+
   return (
     <section className="px-8 py-3 ">
       <div className="mb-5">
--- cellmart-fe/src/components/MobileListing.jsx
+++ cellmart-fe/src/components/MobileListing.jsx
@@ -19,6 +19,56 @@ export default function MobileListings() {
     return mobile.brand.toLowerCase().includes(selectedbrand.toLowerCase());
   })
 
+  if(isLoading){
+    return (
+      <section className="px-8 py-8 lg:py-12">
+        <div className="mb-5">
+          <h2 className="text-3xl md:text-4xl font-bold mb-4">
+            Top trending mobile phones
+          </h2>
+          <p className="text-lg text-muted-foreground">
+            Discover the prefect mobile phone for you
+          </p>
+        </div>
+        <div className="flex items-center gap-x-4">
+          {
+            brands.map((brand) => {
+              return (<BrandTab selectedbrand={selectedbrand} name={brand} onClick={handleSelectedBrand} />)
+            })
+          }
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-4">
+          <p>Loading...</p>
+        </div>
+      </section>
+    );
+  }
+
+  if(isError){
+    return (
+      <section className="px-8 py-8 lg:py-12">
+        <div className="mb-5">
+          <h2 className="text-3xl md:text-4xl font-bold mb-4">
+            Top trending mobile phones
+          </h2>
+          <p className="text-lg text-muted-foreground">
+            Discover the prefect mobile phone for you
+          </p>
+        </div>
+        <div className="flex items-center gap-x-4">
+          {
+            brands.map((brand) => {
+              return (<BrandTab selectedbrand={selectedbrand} name={brand} onClick={handleSelectedBrand} />)
+            })
+          }
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-4">
+        <p>Error Loading</p>
+        </div>
+      </section>
+    );
+  }
+
   return (
     <section className="px-8 py-8 lg:py-12">
       <div className="mb-5">
