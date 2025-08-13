210c9f79c3eadb72ace8956be0328cbabdc95f91
Goyumx <goyum.doingcode@gmail.com>
2025-08-13 23:40:20 +0530

    feat: DTO, req.auth use for reservation

--- cellmart-be/src/application/reservation.ts
+++ cellmart-be/src/application/reservation.ts
@@ -4,23 +4,6 @@ import NotFoundError from "../domain/errors/not-found-error";
 import ValidationError from "../domain/errors/validation-error";
 import { ReservationDTO } from "../domain/dtos/reservation";
 
-
-// Extend Express Request interface to include auth property
-declare global {
-    namespace Express {
-        interface Request {
-            auth?: {
-                userId: string;
-                sessionClaims?: {
-                    metadata: {
-                        role?: "admin";
-                    };
-                };
-            };
-        }
-    }
-}
-
 export const getAllReservations = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const reservations = await Reservation.find();
@@ -57,16 +40,20 @@ export const deleteReservation = async (req: Request, res: Response, next: NextF
 export const createReservation = async (req: Request, res: Response, next: NextFunction) => {
     try {
         
-        const reservation = req.body;
+        const reservation = ReservationDTO.safeParse(req.body);
+
+        if (!reservation.success) {
+            throw new ValidationError(reservation.error.message)
+        }
         const user = req.auth;
 
         await Reservation.create({
-            productId: reservation.productId,
-            productType: reservation.productType,
-            userId: reservation.userId,
-            name: reservation.name,
-            contactNumber: reservation.contactNumber,
-            gmail: reservation.gmail
+            productId: reservation.data.productId,
+            productType: reservation.data.productType,
+            userId: user?.userId,
+            name: reservation.data.name,
+            contactNumber: reservation.data.contactNumber,
+            gmail: reservation.data.gmail
         });
         res.status(201).send();
     } catch (error) {
--- cellmart-be/src/infastructure/global.d.ts
+++ cellmart-be/src/infastructure/global.d.ts
@@ -3,10 +3,18 @@ export {};
 // Create a type for the roles
 export type Role = "admin";
 
+// Extend Express Request interface to include auth property
 declare global {
-  interface CustomJwtSessionClaims {
-    metadata: {
-      role?: Role;
-    };
+  namespace Express {
+      interface Request {
+          auth?: {
+              userId: string;
+              sessionClaims?: {
+                  metadata: {
+                      role?: "admin";
+                  };
+              };
+          };
+      }
   }
 }
