18bf3e8435bc854ee701c75acf2924b82b542162
Goyumx <goyum.doingcode@gmail.com>
2025-08-13 23:55:05 +0530

    feat: token bearer set in FE

--- cellmart-fe/src/lib/api.js
+++ cellmart-fe/src/lib/api.js
@@ -5,7 +5,15 @@ const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${BASE_URL}/api/`}),
+    baseUrl: `${BASE_URL}/api/`,
+    prepareHeaders: async (headers, { getState }) => {
+      const token = await window?.Clerk?.session?.getToken();
+      console.log(token);
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+    }
+  }),
   endpoints: (builder) => ({
     getPhones: builder.query({
       query: () => "phones",
